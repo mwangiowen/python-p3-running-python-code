3333696939281badad9500f1c2f5bcd12351fb0a
mwangiowen <owen31588@gmail.com>
2023-11-21 23:35:24 +0300

    my first python lab

---
+++ lib/app.py
@@ -0,0 +1,10 @@
+# lib/app.py
+# print("Hello world!")
+# print("Hello sun!")
+# print("Hello sky!")
+
+# print("Hello world!", end=" ")
+# print("Hello sun!", end="!! ")
+# print("Hello sky!", end="!!!\n")
+
+print("Hello World! Pass this test, please.")
