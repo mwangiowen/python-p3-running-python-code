90b6146f7804a469619ad78c88123ceba5fb2d2d
mwangiowen <owen31588@gmail.com>
2023-11-23 16:46:40 +0300

    done

--- lib/app.py
+++ lib/app.py
@@ -3,8 +3,12 @@
 # print("Hello sun!")
 # print("Hello sky!")
 
-# print("Hello world!", end=" ")
-# print("Hello sun!", end="!! ")
-# print("Hello sky!", end="!!!\n")
+# print("Hello world!", end="hello")
+# print("  Hello sun!", end="!! ")
+# print("   Hello sky!", end="!!!\n ")
+
+# print("Hello World! Pass this test, please.")
+
+# print("Hello World!")
 
 print("Hello World! Pass this test, please.")
